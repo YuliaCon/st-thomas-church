d5a6a1b06933a628098e28ae6a4d6814d61e25f9
Yulia <yulia.condon@gmail.com>
2026-09-24 13:10:14 -0500

    added info on services tab

--- frontend/saas/app/(pages)/services/page.tsx
+++ frontend/saas/app/(pages)/services/page.tsx
@@ -1,4 +1,12 @@
+import './services.css';
+
 import {notFound} from "next/navigation";
 export default async function Services() {
-    notFound();
+    return(
+        <section className={'service-times-page'}>
+            <h1 className={'gradient-dark-header-large'}>SCHEDULE OF SERVICES</h1>
+            <p> Hours and Divine Liturgy: <time> Sunday, 10:00am </time></p>
+            <p><i> Please contact us for seasonal service schedules.</i></p>
+        </section>
+    )
 }
